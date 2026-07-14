7d5ec8f5377d37d4b9c77e4afdaae52a484146f6
danielpy1985 <94938064+danielpy1985@users.noreply.github.com>
2026-07-13 22:50:17 -0300

    Delete api directory

--- api/verify.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-    const { code } = req.query;
-    if (!code) return res.json({ used: true });
-    return res.json({ used: false });   // 暫時先這樣
-}
